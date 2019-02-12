8b0ac77241643c4f7753ee76ea1c100354972363
daisuke <marondisk.00131@gmail.com>
2019-02-12 18:13:35 +0900

    dddd

--- js/top_scroll.js
+++ js/top_scroll.js
@@ -7,8 +7,17 @@ $(function () {
       $('#top_scroll').fadeOut();
     }
   });
-  $('#top_scroll').click(function () { 
+  $('#top_scroll').click(function () {
     $('html, body').animate({scrollTop: 0}, 500);
+    return false;
+  });
+
+
+  $('#contactbtn, #infobtn-bg').click(function () {
+    // $('#contact-wrapper').scrollTop();
+    $("html,body").animate({scrollTop:$('#contact-wrapper').offset().top});
+    // scrollTo('#contact-wrapper');
+    // $('#contact-wrapper').animate({scrollTop: 0}, 500);
     return false;
   });
 });
